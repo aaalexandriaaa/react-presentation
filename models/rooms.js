c8abe7ddc67c2d64f1f4168393eff6f88576a7f2
aaalexandriaaa <67809768+aaalexandriaaa@users.noreply.github.com>
2020-09-17 11:05:47 -0400

    add roomm and schedule schema

---
+++ models/rooms.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const scheduleSchema = new Schema({
+    user: { type: Schema.Types.ObjectId, ref: 'User' },
+    time: { type: [Number], required: true },
+    peanutGallery: [String],
+    meetingName: String,
+    agendaBody: String
+}, {
+    timestamps: true
+})
+
+const roomSchema = new Schema({
+    name: { type: String, required: true },
+    occupancy: { type: Number, required: true },
+    schedule: [scheduleSchema]
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Room', roomSchema);
