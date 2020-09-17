030ae9d9c86579b44467709437165758c4d3ccd6
cem aytac <cenko@MacBook-Air-3.local>
2020-09-17 14:10:58 -0400

    add roomcard component

--- src/pages/AllRooms/AllRooms.jsx
+++ src/pages/AllRooms/AllRooms.jsx
@@ -1,14 +1,25 @@
 import React, { Component } from 'react'
+import RoomCard from "../../components/RoomCard/RoomCard"
 
 class AllRooms extends Component {
-    state = {}
-    render() {
-        return (
-            <>
-                <h1>ALL ROOMS HERE GET YER ROOMS</h1>
-            </>
-        );
-    }
+  state = {}
+  render() {
+    return (
+      <>
+        <h1>ALL ROOMS HERE GET YER ROOMS</h1>
+        <div className="">
+          {this.props.rooms.map(room =>
+            <RoomCard
+              key={room._id}
+              room={room}
+            // handleDeleteMovie={props.handleDeleteMovie}
+            // user={props.user}
+            />
+          )}
+        </div>
+      </>
+    );
+  }
 }
 
 export default AllRooms;
