3c1d3a5592cd2df774ddb611b67823fb710ea503
Jam Wheat <jamonwheat@gmail.com>
2020-09-17 13:47:41 -0400

    add meetings in back end

---
+++ routes/meetings.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const meetingsCtrl = require('../controllers/meetings');
+
+/*---------- Public Routes ----------*/
+
+
+/*---------- Protected Routes ----------*/
+router.use(require("../config/auth"));
+router.post("/", checkAuth, meetingsCtrl.create);
+
+
+/*---------- Auth Checker ----------*/
+function checkAuth(req, res, next) {
+  if (req.user) return next();
+  return res.status(401).json({msg: 'Not Authorized'});
+}
+
+module.exports = router;
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ require('./config/database');
 const userRouter = require('./routes/users');
 const authRouter = require('./routes/auth');
 const roomsRouter = require('./routes/rooms');
+const meetingsRouter = require('./routes/meetings');
 
 const cors = require('cors')
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.use('/api/auth', authRouter);
 app.use('/api/users', userRouter);
 app.use('/api/rooms', roomsRouter);
+app.use('/api/meetings', meetingsRouter);
 
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
