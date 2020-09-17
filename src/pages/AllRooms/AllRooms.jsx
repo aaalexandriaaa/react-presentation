aaff54ce3695ac86a771624554041411d645f956
aaalexandriaaa <67809768+aaalexandriaaa@users.noreply.github.com>
2020-09-17 11:49:49 -0400

    stub up pages and update nav bar

---
+++ src/pages/AllRooms/AllRooms.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class AllRooms extends Component {
+    state = {}
+    render() {
+        return (
+            <>
+                <h1>ALL ROOMS HERE GET YER ROOMS</h1>
+            </>
+        );
+    }
+}
+
+export default AllRooms;
---
+++ src/pages/ViewMeeting/ViewMeeting.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+class ViewMeeting extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <h1>MEETING DETAILS</h1>
+         );
+    }
+}
+ 
+export default ViewMeeting;
